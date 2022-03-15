2cba6f8a40490774d6131b5225984a3b9ccef55f
Ben Vining <ben.the.vining@gmail.com>
2022-03-15 13:11:07 -0500

    fix: updating config files
    Precommit-Verified: c1ef00ebb5d4c8a3eccad783f60868d16050e177d4580a725fa86601b0671763

--- scripts/alphabetize_codeowners.py
+++ scripts/alphabetize_codeowners.py
@@ -18,23 +18,23 @@ import os
 
 def main():
 
-    LEMONS_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+	LEMONS_ROOT = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
-    CODEOWNERS = os.path.join(LEMONS_ROOT, '.github', 'CODEOWNERS')
+	CODEOWNERS = os.path.join(LEMONS_ROOT, '.github', 'CODEOWNERS')
 
-    with open(CODEOWNERS, 'r') as f:
-        file_lines = f.readlines()
+	with open(CODEOWNERS, 'r') as f:
+		file_lines = f.readlines()
 
-    # remove empty lines
-    file_lines = list(filter(None, file_lines))
+	# remove empty lines
+	file_lines = list(filter(None, file_lines))
 
-    # sort remaining lines
-    file_lines.sort()
+	# sort remaining lines
+	file_lines.sort()
 
-    with open(CODEOWNERS, 'w') as f:
-        f.truncate(0)
-        f.write(''.join(file_lines))
+	with open(CODEOWNERS, 'w') as f:
+		f.truncate(0)
+		f.write(''.join(file_lines))
 
 
 if __name__ == '__main__':
-    main()
+	main()
